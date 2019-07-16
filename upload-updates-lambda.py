b8f77789c9041f18fba718c6a680e5eba25df82e
damengrandom <damon@airteam.com.au>
2019-07-16 16:58:25 +1000

    [SP-04][Damon] updated automation script python file

--- upload-updates-lambda.py
+++ upload-updates-lambda.py
@@ -4,16 +4,21 @@ import StringIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
-serverless_bucket = s3.Bucket("serverless.damengccp.com")
-build_bucket = s3.Bucket('serverlessbuild.damengccp.com')
-serverlessBuild_zip = StringIO.StringIO()
-build_bucket.download_fileobj('serverlessBuild.zip', serverlessBuild_zip)
-with zipfile.ZipFile(serverlessBuild_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        serverless_bucket.upload_fileobj(
-            obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        serverless_bucket.Object(nm).Acl().put(ACL='public-read')
+
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    serverless_bucket = s3.Bucket("serverless.damengccp.com")
+    build_bucket = s3.Bucket('serverlessbuild.damengccp.com')
+    serverlessBuild_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('serverlessBuild.zip', serverlessBuild_zip)
+
+    with zipfile.ZipFile(serverlessBuild_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            serverless_bucket.upload_fileobj(
+                obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            serverless_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    return 'Hi there'
 
 # How to run the code? In terminal pls type: /usr/local/bin/python2 upload-updates-lambda.py
