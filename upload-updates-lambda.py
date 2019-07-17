e4a5617a736794b81904fed90d148ded1755b223
damengrandom <damon@airteam.com.au>
2019-07-17 10:35:52 +1000

    [SP-05][Damon] added SNS service script into python file

--- upload-updates-lambda.py
+++ upload-updates-lambda.py
@@ -6,18 +6,29 @@ import mimetypes
 
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
-    serverless_bucket = s3.Bucket("serverless.damengccp.com")
-    build_bucket = s3.Bucket('serverlessbuild.damengccp.com')
-    serverlessBuild_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('serverlessBuild.zip', serverlessBuild_zip)
+    sns = boto3.resource('sns')
+    topic = sns.Topic(
+        "arn:aws:sns:us-east-1:878359838010:deployServerlessTopic")
+    try:
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+        serverless_bucket = s3.Bucket("serverless.damengccp.com")
+        build_bucket = s3.Bucket('serverlessbuild.damengccp.com')
+        serverlessBuild_zip = StringIO.StringIO()
+        build_bucket.download_fileobj(
+            'serverlessBuild.zip', serverlessBuild_zip)
 
-    with zipfile.ZipFile(serverlessBuild_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            serverless_bucket.upload_fileobj(
-                obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            serverless_bucket.Object(nm).Acl().put(ACL='public-read')
+        with zipfile.ZipFile(serverlessBuild_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                serverless_bucket.upload_fileobj(
+                    obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                serverless_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        topic.publish(Subject="Deployment Success",
+                      Message="Hi there, serverless.damengccp.com has been deployed successfully.")
+    except:
+        topic.publish(Subject="Deployment Failure",
+                      Message="Oops, serverless.damengccp.com hasn't been deployed successfully..")
 
     return 'Hi there'
 
